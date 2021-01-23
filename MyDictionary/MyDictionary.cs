803dae4c59e8506a27446195cfd18fa7a115c1e5
NURDOGAN YAVUZ <nurdogan.yavuz@ogrenci.karatay.edu.tr>
2021-01-23 20:37:34 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        //anahtarlar ve değerler için 2 ayrı array oluşturma
+        //sözlüğün arkaplanında veriler bu dizilerde tutulacak.
+
+        TKey[] keys;
+        TValue[] values;
+
+        //constructor --> yeni sözlük olusturdugumda çalışacak blok
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value) //hangi tipte key veya value girersek, o tipe göre çalışır.
+        {
+            //ekleme işleminde keys ve values dizileri için bellekte yeni adres oluşacagından, dizileri newlemeden önce
+            //bellekteki adreslerini geçici dizilere tuttururuz. 
+            //Yani şu an tempArrayKey bellekte keys dizisinin adresini tutar, tempArrayValue ise values dizisinin adresini tutar.
+            //newlendiği andan itibaren bu diziler baska adreslere atanacagı için, eski adresleri kaybolmasın diye geçici arraylere tuttururuz.
+
+            TKey[] tempArrayKey = keys; 
+            TValue[] tempArrayValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[i] = tempArrayKey[i];
+            }
+            keys[keys.Length - 1] = key; //sözlüğün sonunda eklemek için son index'e atarız girilen key'i
+
+            for (int j = 0; j < tempArrayValue.Length; j++)
+            {
+                values[j] = tempArrayValue[j];
+            }
+            values[values.Length - 1] = value; //sözlüğün sonunda eklemek için son index'e atarız girilen value'u.
+        }
+
+        public void GetAll() //key ve valueları listeleme metodu
+        {
+            for (int i = 0; i < keys.Length; i++) //keys.length ya da values.length kullanırız; çünkü ikisinin boyutu da aynı. 
+            {                                     //ne kadar keys o kadar value :)
+
+                Console.WriteLine("Key: " + keys[i] + " | Value: " + values[i]);
+
+            }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            myDictionary.Add("Böğürtlen", 40);
+            myDictionary.Add("Ananas", 50);
+            myDictionary.Add("Armut", 60);
+            myDictionary.Add("Karpuz", 30);
+            myDictionary.Add("Nar", 80);
+
+            myDictionary.GetAll();
+        }
+    }
+}
